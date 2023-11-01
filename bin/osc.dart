c9cba173a6cdbcb04d06793c15db3211e565999b
grogersstephen <grogersstephen@gmail.com>
2023-11-01 16:02:02 -0500

    initial commit

---
+++ bin/osc.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'package:osc/osc.dart';
+import 'package:osc/message.dart';
+
+
+void main() async {
+  bool ok;
+  Message reply;
+  var conn = Conn("45.56.112.149");
+  await conn.initSender();
+
+  var msg = Message("/ch/01/mix/fader");
+  msg.addFloat(.29);
+
+  ok = await conn.send(msg);
+  if (!ok) {
+	  stdout.write("could not send msg");
+	  return;
+  }
+
+  (reply, ok) = await conn.receive(Duration(seconds:1));
+
+  reply.parse();
+
+  print("tags: ${reply.tags}");
+  print("reply: ${reply.data}");
+
+  conn.sender!.close();
+}
+
+
---
+++ lib/osc.dart
@@ -0,0 +1,72 @@
+import 'dart:core';
+import 'dart:io';
+import 'package:udp/udp.dart';
+import 'message.dart';
+
+class Conn {
+	String remoteHost;
+	int remotePort = 10023;
+	int localPort = 10023;
+	UDP? sender;
+	Endpoint? dest;
+
+	Conn(this.remoteHost) {
+		if (!initDest()) {
+			stdout.write("unable to make remote endpoint");
+		}
+	}
+
+	bool initDest() {
+		try {
+			dest = Endpoint.unicast(InternetAddress(remoteHost), port: Port(remotePort));
+		} catch(e) {
+			return false;
+		}
+		return true;
+	}
+
+	Future<bool> initSender() async {
+		// Close sender if one is already open
+		try {
+			sender!.close();
+		} catch(e) {}
+
+		// Assign sender
+		try {
+			sender = await UDP.bind(Endpoint.any(port: Port(localPort)));
+		} catch(e) {
+			return false;
+		}
+		return true;
+	}
+
+	Future<bool> send(Message message) async {
+		// Make the packet from the message
+		message.makePacket();
+		// Send the data
+		try {
+			final dataLength = await sender!.send(message.packet, dest!);
+			if (dataLength < 1) {
+				return false;
+			}
+		} catch(e) {
+			return false;
+		}
+		return true;
+	}
+
+	Future<(Message, bool)> receive(Duration timeout) async {
+		var msg = Message("");
+		bool ok = false;
+		try {
+		sender!.asStream(timeout: timeout).listen((datagram) {
+			msg.packet = datagram!.data;
+			ok = true;
+		});
+		} catch(e) {
+			return (msg, ok);
+		}
+  	    await Future.delayed(timeout);
+		return (msg, ok);
+	}
+}
