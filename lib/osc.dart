58ae5a6895818c071832107436fb0ab6b16b8241
joseph <josephmpalacio@gmail.com>
2023-11-01 20:28:58 -0500

    simplify

--- lib/osc.dart
+++ lib/osc.dart
@@ -1,72 +1,39 @@
+library osc;
+
 import 'dart:core';
 import 'dart:io';
 import 'package:udp/udp.dart';
 import 'message.dart';
 
 class Conn {
-	String remoteHost;
-	int remotePort = 10023;
-	int localPort = 10023;
-	UDP? sender;
-	Endpoint? dest;
+  late final String _remoteHost;
+  late final int _remotePort;
+  late final int _localPort;
+  // late UDP _sender;
+  late Endpoint _dest;
 
-	Conn(this.remoteHost) {
-		if (!initDest()) {
-			stdout.write("unable to make remote endpoint");
-		}
-	}
+  Conn({required remoteHost, remotePort = 10023, localPort = 10023})
+      : _remoteHost = remoteHost,
+        _remotePort = remotePort,
+        _localPort = localPort {
+    try {
+      _dest = Endpoint.multicast(InternetAddress(_remoteHost),
+          port: Port(_remotePort));
+    } catch (e) {
+      rethrow;
+    }
+  }
 
-	bool initDest() {
-		try {
-			dest = Endpoint.unicast(InternetAddress(remoteHost), port: Port(remotePort));
-		} catch(e) {
-			return false;
-		}
-		return true;
-	}
-
-	Future<bool> initSender() async {
-		// Close sender if one is already open
-		try {
-			sender!.close();
-		} catch(e) {}
-
-		// Assign sender
-		try {
-			sender = await UDP.bind(Endpoint.any(port: Port(localPort)));
-		} catch(e) {
-			return false;
-		}
-		return true;
-	}
-
-	Future<bool> send(Message message) async {
-		// Make the packet from the message
-		message.makePacket();
-		// Send the data
-		try {
-			final dataLength = await sender!.send(message.packet, dest!);
-			if (dataLength < 1) {
-				return false;
-			}
-		} catch(e) {
-			return false;
-		}
-		return true;
-	}
-
-	Future<(Message, bool)> receive(Duration timeout) async {
-		var msg = Message("");
-		bool ok = false;
-		try {
-		sender!.asStream(timeout: timeout).listen((datagram) {
-			msg.packet = datagram!.data;
-			ok = true;
-		});
-		} catch(e) {
-			return (msg, ok);
-		}
-  	    await Future.delayed(timeout);
-		return (msg, ok);
-	}
+  Future<void> send(Message message) async {
+    final sender = await UDP.bind(Endpoint.any(port: Port(_localPort)));
+    // Make the packet from the message
+    message.makePacket();
+    // Send the data
+    try {
+      final dataLength = await sender.send(message.packet, _dest);
+      if (dataLength == 0) throw Error.safeToString("too short");
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
--- test/osc_test.dart
+++ test/osc_test.dart
@@ -2,7 +2,7 @@ import 'package:osc/osc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('conn', () {
+    expect(Conn(remoteHost: "192.168.50.1"), 42);
   });
 }
