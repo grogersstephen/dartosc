741c5f7e9e0a3d13478273d75bca41f6bb1834c6
unknown <josephmpalacio@gmail.com>
2023-11-16 20:17:15 -0600

    add stream

--- bin/client.dart
+++ bin/client.dart
@@ -1,15 +1,31 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:osc/message.dart';
 import 'package:osc/osc.dart';
 
 void main() async {
-  // var conn = await UDP.bind(Endpoint.any(port: Port(65000)));
+  final p = ReceivePort();
+  await Isolate.spawn(_receive, p.sendPort);
+  late SendPort sendPort;
+  // The first message from the spawned isolate is a SendPort. This port is
+  // used to communicate with the spawned isolate.
+  try {
+    sendPort = await p.first;
+  } catch (e) {
+    print("try to send the first send port: $e");
+  }
+
   var conn = await Conn.initUDP(
       remoteHost: "127.0.0.1", remotePort: 65002, localPort: 65000);
-  // // send a simple string to a broadcast endpoint on port 65001.
-  // var dataLength = await sender.send(
-  //     'Hello World!'.codeUnits, Endpoint.loopback(port: Port(65002)));
+
+  final sub = conn.listen();
+
+  sub.onData((data) {
+    sendPort.send(data);
+  });
+
   while (true) {
     stdout.write("input: ");
     final val = stdin.readLineSync();
@@ -18,6 +34,25 @@ void main() async {
     final i = await conn.send(Message(val));
     print('bytes written: $i');
     final resp = await conn.receive();
-    print('resp: $resp');
+    print('received: $resp');
   }
+}
+
+Future<void> _receive(SendPort p) async {
+  final commandPort = ReceivePort();
+  p.send(commandPort.sendPort);
+
+  // Wait for messages from the main isolate.
+  await for (final message in commandPort) {
+    if (message is Message) {
+      // Read and decode the file.
+
+      print('Isolate resp: $message');
+    } else if (message == null) {
+      // Exit if the main isolate sends a null message, indicating there are no
+      // more files to read and parse.
+      break;
+    }
+  }
+  Isolate.exit();
 }
--- lib/osc.dart
+++ lib/osc.dart
@@ -1,5 +1,6 @@
 library osc;
 
+import 'dart:async';
 import 'dart:core';
 import 'dart:io';
 import 'package:udp/udp.dart';
@@ -9,6 +10,7 @@ import 'dart:typed_data';
 class Conn {
   final Endpoint _dest;
   final UDP _sender;
+  final StreamController<Message> _streamController = StreamController();
 
   Conn({required Endpoint dest, required UDP sender})
       : _dest = dest,
@@ -21,7 +23,16 @@ class Conn {
     final dest =
         Endpoint.unicast(InternetAddress(remoteHost), port: Port(remotePort));
     final sender = await UDP.bind(Endpoint.any(port: Port(localPort)));
-    return Conn(dest: dest, sender: sender);
+    final conn = Conn(dest: dest, sender: sender);
+
+// stream stuff....
+    final messages = sender
+        .asStream()
+        .map((event) => Message.fromPacket(event?.data ?? Uint8List(0)));
+
+    conn._streamController.addStream(messages);
+
+    return conn;
   }
 
   get sender => _sender;
@@ -33,10 +44,15 @@ class Conn {
     try {
       final event = await _sender.asStream(timeout: timeout).first;
       var data = event?.data ?? Uint8List(0);
+
       return Message.fromPacket(data);
     } catch (e) {
       rethrow;
     }
+  }
+
+  StreamSubscription<Message> listen() {
+    return _streamController.stream.listen(null);
   }
 
   Future<int> send(Message message) async {
